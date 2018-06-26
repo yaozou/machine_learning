ea01935b7ddac7b7b75f35d2d7f2f64d4af47558
yaozou <771538058@qq.com>
2018-06-26 22:00:29 +0800

    优惠券

---
+++ o2o_coupon/test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+ipl_data1 = {'user_id': [1002625, 100330, 1003825,1008100,1010689],
+         'coupon_id': [4806, 8635, 13870,4727,7965],
+         'max_date_received': [20160726,20160703,20160731,20160721,20160718],
+         'min_date_received':[20160712,20160702,20160716,20160711,20160712]}
+t2 = pd.DataFrame(ipl_data1)
+print (t2)
+
+ipl_data2 = {'user_id': [4129537, 6949378, 2166529,2166529],
+         'coupon_id': [9983, 3429, 6928,1808],
+         'date_received': [20160712,20160706,20160727,20160727]}
+t3 = pd.DataFrame(ipl_data2)
+print (t3)
+t3 = pd.merge(t3, t2, on=['user_id', 'coupon_id'], how='left')
+print (t3)
