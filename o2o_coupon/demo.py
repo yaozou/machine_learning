d35976d3ca4fbfeda791282d4cdb10fee0be0085
yaozou <yaozou@pand-auto.com>
2018-08-24 16:49:13 +0800

    简化代码

--- o2o_coupon/demo.py
+++ o2o_coupon/demo.py
@@ -153,7 +153,7 @@ def get_day_gap_after(s):
     else:
         return min(gaps)
 
-def dealDataset3(dataset3):
+def dealOtherFeature(dataset3,file_name):
     # dataset3
     t = dataset3[['user_id']]
     t['this_month_user_receive_all_coupon_count'] = 1
@@ -211,129 +211,13 @@ def dealDataset3(dataset3):
     other_feature3 = pd.merge(other_feature3, t4, on=['user_id', 'date_received']).dropna(how='any')
     other_feature3 = pd.merge(other_feature3, t5, on=['user_id', 'coupon_id', 'date_received']).dropna(how='any')
     other_feature3 = pd.merge(other_feature3, t7, on=['user_id', 'coupon_id', 'date_received']).dropna(how='any')
-    other_feature3.to_csv('data/other_feature3.csv', index=None)
-    print(other_feature3.shape)
+    other_feature3.to_csv(file_name, index=None)
+    return other_feature3
 
-def dealDataset2(dataset2):
-    # for dataset2
-    t = dataset2[['user_id']]
-    t['this_month_user_receive_all_coupon_count'] = 1
-    t = t.groupby('user_id').agg('sum').reset_index()
 
-    t1 = dataset2[['user_id', 'coupon_id']]
-    t1['this_month_user_receive_same_coupon_count'] = 1
-    t1 = t1.groupby(['user_id', 'coupon_id']).agg('sum').reset_index()
-
-    t2 = dataset2[['user_id', 'coupon_id', 'date_received']]
-    t2.date_received = t2.date_received.astype('str')
-    t2 = t2.groupby(['user_id', 'coupon_id'])['date_received'].agg(lambda x: ':'.join(x)).reset_index()
-    t2['receive_number'] = t2.date_received.apply(lambda s: len(s.split(':')))
-    t2 = t2[t2.receive_number > 1]
-    t2['max_date_received'] = t2.date_received.apply(lambda s: max([int(d) for d in s.split(':')]))
-    t2['min_date_received'] = t2.date_received.apply(lambda s: min([int(d) for d in s.split(':')]))
-    t2 = t2[['user_id', 'coupon_id', 'max_date_received', 'min_date_received']]
-
-    t3 = dataset2[['user_id', 'coupon_id', 'date_received']]
-    t3 = pd.merge(t3, t2, on=['user_id', 'coupon_id'], how='left')
-    t3['this_month_user_receive_same_coupon_lastone'] = t3.max_date_received - t3.date_received.astype('int')
-    t3['this_month_user_receive_same_coupon_firstone'] = t3.date_received.astype('int') - t3.min_date_received
-    t3.this_month_user_receive_same_coupon_lastone = t3.this_month_user_receive_same_coupon_lastone.apply(
-        is_firstlastone)
-    t3.this_month_user_receive_same_coupon_firstone = t3.this_month_user_receive_same_coupon_firstone.apply(
-        is_firstlastone)
-    t3 = t3[['user_id', 'coupon_id', 'date_received', 'this_month_user_receive_same_coupon_lastone',
-             'this_month_user_receive_same_coupon_firstone']]
-
-    t4 = dataset2[['user_id', 'date_received']]
-    t4['this_day_user_receive_all_coupon_count'] = 1
-    t4 = t4.groupby(['user_id', 'date_received']).agg('sum').reset_index()
-
-    t5 = dataset2[['user_id', 'coupon_id', 'date_received']]
-    t5['this_day_user_receive_same_coupon_count'] = 1
-    t5 = t5.groupby(['user_id', 'coupon_id', 'date_received']).agg('sum').reset_index()
-
-    t6 = dataset2[['user_id', 'coupon_id', 'date_received']]
-    t6.date_received = t6.date_received.astype('str')
-    t6 = t6.groupby(['user_id', 'coupon_id'])['date_received'].agg(lambda x: ':'.join(x)).reset_index()
-    t6.rename(columns={'date_received': 'dates'}, inplace=True)
-
-    t7 = dataset2[['user_id', 'coupon_id', 'date_received']]
-    t7 = pd.merge(t7, t6, on=['user_id', 'coupon_id'], how='left')
-    t7['date_received_date'] = t7.date_received.astype('str') + '-' + t7.dates
-    t7['day_gap_before'] = t7.date_received_date.apply(get_day_gap_before)
-    t7['day_gap_after'] = t7.date_received_date.apply(get_day_gap_after)
-    t7 = t7[['user_id', 'coupon_id', 'date_received', 'day_gap_before', 'day_gap_after']]
-
-    other_feature2 = pd.merge(t1, t, on='user_id')
-    other_feature2 = pd.merge(other_feature2, t3, on=['user_id', 'coupon_id'])
-    other_feature2 = pd.merge(other_feature2, t4, on=['user_id', 'date_received'])
-    other_feature2 = pd.merge(other_feature2, t5, on=['user_id', 'coupon_id', 'date_received'])
-    other_feature2 = pd.merge(other_feature2, t7, on=['user_id', 'coupon_id', 'date_received'])
-    other_feature2.to_csv('data/other_feature2.csv', index=None)
-    print(other_feature2.shape)
-
-def dealDataset1(dataset1):
-    # for dataset1
-    t = dataset1[['user_id']]
-    t['this_month_user_receive_all_coupon_count'] = 1
-    t = t.groupby('user_id').agg('sum').reset_index()
-
-    t1 = dataset1[['user_id', 'coupon_id']]
-    t1['this_month_user_receive_same_coupon_count'] = 1
-    t1 = t1.groupby(['user_id', 'coupon_id']).agg('sum').reset_index()
-
-    t2 = dataset1[['user_id', 'coupon_id', 'date_received']]
-    t2.date_received = t2.date_received.astype('str')
-    t2 = t2.groupby(['user_id', 'coupon_id'])['date_received'].agg(lambda x: ':'.join(x)).reset_index()
-    t2['receive_number'] = t2.date_received.apply(lambda s: len(s.split(':')))
-    t2 = t2[t2.receive_number > 1]
-    t2['max_date_received'] = t2.date_received.apply(lambda s: max([int(d) for d in s.split(':')]))
-    t2['min_date_received'] = t2.date_received.apply(lambda s: min([int(d) for d in s.split(':')]))
-    t2 = t2[['user_id', 'coupon_id', 'max_date_received', 'min_date_received']]
-
-    t3 = dataset1[['user_id', 'coupon_id', 'date_received']]
-    t3 = pd.merge(t3, t2, on=['user_id', 'coupon_id'], how='left')
-    t3['this_month_user_receive_same_coupon_lastone'] = t3.max_date_received - t3.date_received.astype('int')
-    t3['this_month_user_receive_same_coupon_firstone'] = t3.date_received.astype('int') - t3.min_date_received
-
-    t3.this_month_user_receive_same_coupon_lastone = t3.this_month_user_receive_same_coupon_lastone.apply(
-        is_firstlastone)
-    t3.this_month_user_receive_same_coupon_firstone = t3.this_month_user_receive_same_coupon_firstone.apply(
-        is_firstlastone)
-    t3 = t3[['user_id', 'coupon_id', 'date_received', 'this_month_user_receive_same_coupon_lastone',
-             'this_month_user_receive_same_coupon_firstone']]
-
-    t4 = dataset1[['user_id', 'date_received']]
-    t4['this_day_user_receive_all_coupon_count'] = 1
-    t4 = t4.groupby(['user_id', 'date_received']).agg('sum').reset_index()
-
-    t5 = dataset1[['user_id', 'coupon_id', 'date_received']]
-    t5['this_day_user_receive_same_coupon_count'] = 1
-    t5 = t5.groupby(['user_id', 'coupon_id', 'date_received']).agg('sum').reset_index()
-
-    t6 = dataset1[['user_id', 'coupon_id', 'date_received']]
-    t6.date_received = t6.date_received.astype('str')
-    t6 = t6.groupby(['user_id', 'coupon_id'])['date_received'].agg(lambda x: ':'.join(x)).reset_index()
-    t6.rename(columns={'date_received': 'dates'}, inplace=True)
-
-    t7 = dataset1[['user_id', 'coupon_id', 'date_received']]
-    t7 = pd.merge(t7, t6, on=['user_id', 'coupon_id'], how='left')
-    t7['date_received_date'] = t7.date_received.astype('str') + '-' + t7.dates
-    t7['day_gap_before'] = t7.date_received_date.apply(get_day_gap_before)
-    t7['day_gap_after'] = t7.date_received_date.apply(get_day_gap_after)
-    t7 = t7[['user_id', 'coupon_id', 'date_received', 'day_gap_before', 'day_gap_after']]
-
-    other_feature1 = pd.merge(t1, t, on='user_id')
-    other_feature1 = pd.merge(other_feature1, t3, on=['user_id', 'coupon_id'])
-    other_feature1 = pd.merge(other_feature1, t4, on=['user_id', 'date_received'])
-    other_feature1 = pd.merge(other_feature1, t5, on=['user_id', 'coupon_id', 'date_received'])
-    other_feature1 = pd.merge(other_feature1, t7, on=['user_id', 'coupon_id', 'date_received'])
-    other_feature1.to_csv('data/other_feature1.csv', index=None)
-    print(other_feature1.shape)
-
-dealDataset3(dataset3)
-dealDataset2(dataset2)
-dealDataset1(dataset1)
+dealOtherFeature(dataset3,'data/other_feature3.csv')
+dealOtherFeature(dataset2,'data/other_feature2.csv')
+dealOtherFeature(dataset1,'data/other_feature1.csv')
 
 """
 3、商户相关特征，9个，从训练集1、训练集2和测试集的特征区间提取： 
